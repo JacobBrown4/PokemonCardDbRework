4675a1060372fbcabbb82bbb4fcd2e0baaa46311
Jason Adams <adams.jason.029@gmail.com>
2021-12-14 16:24:10 -0500

    added model for pokemon creature cards

---
+++ PokemonCard.Models/PokemoneCreate.cs
@@ -0,0 +1,17 @@
+﻿using PokemonCard.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonCard.Models
+{
+    public class PokemoneCreate : CardCreate
+    {
+        public PokemonType PokemonType { get; set; }
+        public bool Evolves { get; set; }
+        public string Attack1 { get; set; }
+        public string Attack2 { get; set; }
+    }
+}
